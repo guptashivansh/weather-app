6f4b8caa468697652de58a66eb571f741cd571d3
Shivansh Gupta <gupta.shivansh98@gmail.com>
2019-06-06 22:57:08 +0530

    Trial

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (lat,lng,callback) =>{
         }else if(body.error){
             callback("Unable to forecast",undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary + ' It is currently '+ body.currently.temperature+ " degrees out. There is a " + body.currently.precipProbability + "% chance of rain.")
+            callback(undefined, body.daily.data[0].summary + ' It  is currently '+ body.currently.temperature+ " degrees out. There is a " + body.currently.precipProbability + "% chance of rain.")
         }
     })
 
